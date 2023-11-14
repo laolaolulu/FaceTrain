0410b5251859a5aa20fbeb4592913ea566eb8512
LP <1570800510@qq.com>
2023-11-14 16:06:27 +0800

    备份

---
+++ csharp/OnnxRuntime/ArcFace.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceTrain.OnnxRuntime
+{
+    public class ArcFace
+    {
+        readonly InferenceSession net;
+        readonly int[] dimensions;
+        readonly string input_name;
+        readonly float mean = 127.5f, stddev = 127.5f;
+        public ArcFace(byte[] model)
+        {
+            net = new InferenceSession(model);
+            input_name = net.InputNames[0];
+            dimensions = net.InputMetadata[input_name].Dimensions;
+        }
+
+        public void Run(byte[] img)
+        {
+            using Image<Rgb24> image = Image.Load<Rgb24>(img);
+            image.Mutate(x =>
+            {
+                x.Resize(dimensions[2], dimensions[3]);
+            });
+            dimensions[0] = 1;
+            DenseTensor<float> processedImage = new(dimensions);
+            image.ProcessPixelRows(accessor =>
+            {
+                for (int y = 0; y < accessor.Height; y++)
+                {
+                    Span<Rgb24> pixelSpan = accessor.GetRowSpan(y);
+                    for (int x = 0; x < accessor.Width; x++)
+                    {
+                        processedImage[0, 0, y, x] = (pixelSpan[x].R - mean) / stddev;
+                        processedImage[0, 1, y, x] = (pixelSpan[x].G - mean) / stddev;
+                        processedImage[0, 2, y, x] = (pixelSpan[x].B - mean) / stddev;
+                    }
+                }
+            });
+            //stopwatch.Stop();
+            //Debug.WriteLine("pretreatment:" + stopwatch.Elapsed.TotalMilliseconds);
+
+            using var inputOrtValue = OrtValue.CreateTensorValueFromMemory(OrtMemoryInfo.DefaultInstance,
+                                        processedImage.Buffer, new long[] { 1, dimensions[1], dimensions[2], dimensions[3] });
+
+            var inputs = new Dictionary<string, OrtValue>
+                {
+                    { input_name, inputOrtValue }
+                };
+
+            //执行推理
+            using var results = net.Run(new RunOptions(), inputs, net.OutputNames);
+
+            var tensor = results[0].GetTensorDataAsSpan<float>();
+            float[] embedding = new float[tensor.Length];
+
+            double l2 = 0;
+            for (int i = 0; i < tensor.Length; i++)
+            {
+                embedding[i] = tensor[i];
+                l2 = l2 + Math.Pow(tensor[i], 2);
+            }
+            l2 = Math.Sqrt(l2);
+
+            for (int i = 0; i < embedding.Length; i++)
+                embedding[i] = embedding[i] / (float)l2;
+        }
+    }
+}
---
+++ csharp/Tests/ArcFaceTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FaceTrain.OnnxRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp.Dnn;
+using OpenCvSharp;
+using System.Diagnostics;
+
+namespace FaceTrain.OnnxRuntime.Tests
+{
+    [TestClass()]
+    public class ArcFaceTests
+    {
+        [TestMethod()]
+        public void RunTest()
+        {
+            byte[] modelBytes = File.ReadAllBytes("Resource\\Model\\ArcFace\\cisia_r50_pfc-2021年8月5日.onnx");
+            var net = new OnnxRuntime.ArcFace(modelBytes);
+            var imgsName = Directory.GetFiles("Resource\\Img");
+            var imgs = imgsName.Select(s => (name: s, img: File.ReadAllBytes(s)));
+            // var img = File.ReadAllBytes("Resource\\Img\\test.png");
+            foreach (var item in imgs)
+            {
+                using var image = Cv2.ImDecode(item.img, ImreadModes.Color);
+             //   stopwatch.Restart();
+               net.Run(item.img);
+            //    stopwatch.Stop();
+           //     DrawTextWithBackground(image, "time:" + stopwatch.Elapsed.TotalSeconds.ToString("0.00"), new Point(30, 50));
+                //foreach (var face in faces)
+                //{
+                //    image.Rectangle(Rect.FromLTRB((int)face.Left, (int)face.Top, (int)face.Right, (int)face.Bottom), new Scalar(255));
+                ////    DrawTextWithBackground(image, "score:" + face.Score.ToString("0.00"), new Point(face.Left + 10, face.Top + 30));
+                //    if (face.Landmark != null)
+                //    {
+                //        foreach (var point in face.Landmark)
+                //        {
+                //            image.Circle((int)point.X, (int)point.Y, 2, Scalar.Red, -1);
+                //        }
+                //    }
+                //}
+                //Cv2.ImShow(DateTime.Now.Ticks.ToString(), image);
+            }
+            Cv2.WaitKey(0);
+        }
+    }
+}
